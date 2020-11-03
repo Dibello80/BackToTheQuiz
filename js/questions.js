c40e63d178350d6b01f7af0e708acec29dce9633
Dibello80 <angelodibellomusic@gmail.com>
2020-11-03 01:33:33 -0800

    blueprintApp

---
+++ js/questions.js
@@ -0,0 +1,60 @@
+// creating an array and passing the number, questions, options, and answers
+
+let questions = [
+    {
+    numb: 1,
+    question: "What's the name of actor Michael J. Fox's character in Back to the Future?",
+    answer: "Marty McFly",
+    options: [
+      "Barty McFly",
+      "Farty McFLy",
+      "Marty McFly",
+      "McFly TheFly"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is 'Doc' Brown's first name? ",
+    answer: "Emmett",
+    options: [
+      "Jules",
+      "Verne",
+      "Emmett",
+      "Tommy"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What type of car is converted into a time machine?",
+    answer: "DeLorean",
+    options: [
+      "Dodge",
+      "Delray",
+      "DeLorean",
+      "McLaren"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How many gigawatts does it take to power the Time Machine's flux capacitor?",
+    answer: "1.51 gigawatts",
+    options: [
+      "88 gigawatts",
+      "1.21 gigawatts",
+      "55 gigawatts",
+      "1.51 gigawatts"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the name of the bully in Back to the Future?",
+    answer: "Biff Tannen",
+    options: [
+      "Griff Tannen",
+      "Snitt Tannen",
+      "Whiff Tannen",
+      "Biff Tannen"
+    ]
+  },
+  
+];
